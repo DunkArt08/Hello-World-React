eb991facdcf3990a7dbbaa354c8b7e5ec90f5c23
Daniel Lucas <daniellucaspc@hotmail.com>
2024-08-05 21:52:57 -0300

    First commit

---
+++ src/paginas/home/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './home.css';
+
+function Home(){
+    return(
+        <>
+        <h1 className="titulo">Hello World</h1>
+        <h1 className="titulo">Daniel Lucas Santana Lopes</h1>    
+        <img src="https://images.stockcake.com/public/1/e/f/1efd9cc9-008c-49f8-ba71-d2d92257e3e9_large/sunset-coastal-view-stockcake.jpg" alt="Imagem" className="img" />
+        </>
+    );
+}
+
+export default Home;
